d176626dee8099c5f24087d876da446cd83d48a0
Pranav Siddineni <pranav.siddineni@gmail.com>
2024-10-19 16:52:56 -0400

    Parse user prompt and retrieve API to call.

--- gemini/basicUI.py
+++ gemini/basicUI.py
@@ -5,17 +5,27 @@ Created on Oct 11, 2024
 '''
 
 import os
-import webbrowser
 import google.generativeai as genai
+
 
 GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-user_input = input("What UI would you like to create today? ")
-response = model.generate_content(user_input + " Start your response with \"<!DOCTYPE html>\" and end with \"< html>\"")
+sample_pdf = genai.upload_file("api_database.pdf")
+#summary = model.generate_content(["Give me a summary of this pdf file.", sample_pdf])
+#print(summary)
 
-html_content = response.text
-with open('temp.html', 'w') as f:
-    f.write(html_content)
-webbrowser.open_new_tab('temp.html')
+user_input = input("What UI would you like to create today? ")
+
+# Split user input into different UI elements
+ui_elements = user_input.split(" and ")
+print(ui_elements)
+
+# Get the API for each element the user wants
+for i, val in enumerate(ui_elements):
+    prompt = "Get the UI element(s) for " + val + " from this pdf file. Give me only the endpoint and payload."
+    response = model.generate_content([prompt, sample_pdf])
+    print("Element " + str(i) + "\n" + response.text)
+    
+    # Construct an HTTP POST request with the retrieved JSON
